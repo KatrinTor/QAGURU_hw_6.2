6e484e5166181dc5eca92d683ce9f25b79209279
Ekaterina.Torsunova <katrin.tep@list.ru>
2023-06-07 16:49:06 +0300

    Фикстура вынесена в conftest.py, добавлен тест test_failed_search, обновлен файл requirements.txt.

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope="session", autouse=True)
+def brows_size():
+    browser.config.window_width = 1900
+    browser.config.window_height = 1080
---
+++ tests/homework_6_2.py
@@ -0,0 +1,15 @@
+from selene.support.shared import browser
+from selene import be, have
+
+
+def test_succesful_search():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    print("По запросу найдена ссылка")
+
+def test_failed_search():
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('gjgcxTYYddccv').press_enter()
+    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
+    print("По запросу ничего не найдено")
